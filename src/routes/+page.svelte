a3a04c0f7514059c564ff16eca8301cb5e9a757c
alvinlaroya <alvinreggaelaroya@gmail.com>
2023-08-18 14:01:53 +0800

    feat: link scroll on section
    chore: added gtm
    
    chore: added gtm event

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   import Banner from "../lib/components/sections/Banner.svelte";
   import AboutMe from "../lib/components/sections/AboutMe.svelte";
-  import SpeedDial from "../lib/components/SpeedDial.svelte";
   import Skills from "../lib/components/sections/Skills.svelte";
   import Timeline from "../lib/components/sections/Timeline.svelte";
   import ContactMe from "../lib/components/sections/ContactMe.svelte";
@@ -12,7 +11,6 @@
 </svelte:head>
 <Banner />
 <AboutMe />
-<SpeedDial />
 <Skills />
 <Timeline />
 <ContactMe />
